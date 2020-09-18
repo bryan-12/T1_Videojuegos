a980d5399ac05a28fdae5e9d654ca11f79b8c178
brayan M.P <bryan17.22@outlook.com>
2020-09-18 16:50:28 -0500

    disculpe pero porfin solucione el problema al subirlo al repositorio

---
+++ Exam_T1/Assets/Scripts/KunaiController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KunaiController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(10, rb.velocity.y);
+    }
+}
---
+++ Exam_T1/Assets/Scripts/PersonajeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeController : MonoBehaviour
+{
+    public float jumpForce = 30f;
+    private const int ANIMATION_QUIETO = 0;
+    private const int ANIMATION_CORRER = 1;
+    private const int ANIMATION_SALTAR = 2;
+    public GameObject Kunai;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer sr;
+    private Transform _transform;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        animator.SetInteger("Estado", ANIMATION_QUIETO);
+
+        if (Input.GetKey(KeyCode.RightArrow))  
+        {
+            rb.velocity = new Vector2(10, rb.velocity.y);
+            animator.SetInteger("Estado", ANIMATION_CORRER);
+            sr.flipX = false;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-10, rb.velocity.y);
+            animator.SetInteger("Estado", ANIMATION_CORRER);
+            sr.flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Instantiate(Kunai, _transform.position, Quaternion.identity);
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "enemigo")
+        {
+
+            Debug.Log("Ninja choco con algo");
+
+        }
+    }
+}
